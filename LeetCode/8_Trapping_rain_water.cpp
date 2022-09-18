a77629f0904fbe311d71281195e4e3b5ba818581
_monitdangi_ <monitdangi2562@gmail.com>
2022-09-18 18:54:43 +0530

    trapping rain water

---
+++ LeetCode/8_Trapping_rain_water.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int left=0,right =h.size()-1,left_max=0,right_max=0;
+        int ans =0;
+        while(left<right){
+            if(h[left]<=h[right]){
+                if(left_max<h[left])left_max = h[left];
+                else{
+                    ans += left_max -h[left];
+                }
+                left++;
+            }
+            else{
+                if(right_max<h[right])right_max = h[right];
+                else{
+                    ans += right_max- h[right];
+                }
+                right--;
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+    return 0;
+}
