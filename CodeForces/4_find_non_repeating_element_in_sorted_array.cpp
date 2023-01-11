e3f55400e3d7e57b00f5301b927e3da10af19b8c
_monitdangi_ <monitdangi2562@gmail.com>
2023-01-11 22:06:47 +0530

    Find non repeating element in sorted array

---
+++ CodeForces/4_find_non_repeating_element_in_sorted_array.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i = 0; i<n;i++){
+        int temp;
+        cin>>temp;
+        v.push_back(temp);
+    }
+    int low = 0, high = n-1;
+    while(low<=high){
+        if(low ==  high){
+            cout<<v[low]<<endl;
+            break;
+        }
+        int mid = low + (high-low)/2;
+        if(mid % 2){
+            if(v[mid]!=v[mid-1]){
+                high = mid;
+            }
+            else low = mid+1;
+        }
+        else{
+            if(v[mid]!=v[mid+1]){
+                high = mid;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return 0;
+    }
+}
